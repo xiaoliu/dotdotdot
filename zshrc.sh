68951640846435aadaa7a33700e3279ae5ce42fd
Jack Liu <nafeivom>
2026-02-22 12:37:46 +1100

    upated zshrc.sh

--- zshrc.sh
+++ zshrc.sh
@@ -1,4 +1,7 @@
 # shell utilitiy bits and pieces
 
 # on MacOS opens manpage in preview for easy reading
-function preman() { mandoc -T pdf "$(/usr/bin/man -w $@)" | open -fa Preview } 
+function preman() { mandoc -T pdf "$(/usr/bin/man -w $@)" | open -fa Preview }
+
+#Pretty/Clean git output LESS=FRSX
+export LESS=FRX
